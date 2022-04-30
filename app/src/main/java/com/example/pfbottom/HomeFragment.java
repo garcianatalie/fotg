795afb1720f6d69dc6ae515d2aaccd8264c400c4
Natalie Garcia <ng80165n@pace.edu>
2022-04-29 21:10:22 -0400

    button error

--- app/src/main/java/com/example/pfbottom/HomeFragment.java
+++ app/src/main/java/com/example/pfbottom/HomeFragment.java
@@ -1,28 +1,54 @@
 package com.example.pfbottom;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class HomeFragment extends Fragment {
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
-    }
+    Button btnEnterFreezer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        btnEnterFreezer = v.findViewById(R.id.btnEnterFreezer);
+
+        btnEnterFreezer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFreezer();
+
+            }
+        });
+        return v;
 
     }
+
+// open freezer page
+    private void openFreezer() {
+        Intent i = new Intent(HomeFragment.this.getActivity(), Freezer.class);
+        //Intent i = new Intent(getActivity(), Freezer.class);
+      startActivity(i);
+    }
+
 }
